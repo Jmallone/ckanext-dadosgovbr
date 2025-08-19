fc0360f84bdbe117e297335bf55d06d210a03e7f
JMallone <michels@alunos.utfpr.edu.br>
2025-08-19 00:59:30 -0300

    Fix

--- ckanext/dadosgovbr/helpers/__init__.py
+++ ckanext/dadosgovbr/helpers/__init__.py
@@ -5,3 +5,17 @@ try:
 except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
+
+# Import all helper modules explicitly
+try:
+    from . import tools
+    from . import wordpress
+    from . import scheming
+except ImportError:
+    # If relative imports fail, try absolute imports
+    try:
+        import ckanext.dadosgovbr.helpers.tools
+        import ckanext.dadosgovbr.helpers.wordpress
+        import ckanext.dadosgovbr.helpers.scheming
+    except ImportError:
+        pass
